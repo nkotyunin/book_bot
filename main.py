c0f7df08c54d5f2e6730d8651ff913632a969739
Nikita Kotyunin <nkotyunin@gmail.com>
2024-03-03 13:53:37 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+
+logger = logging.getLogger(__name__)
+
+
+async def main() -> None:
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="[{asctime}] {levelname:8} {filename}:{lineno} - {name} - {message}",
+        style="{",
+    )
+    
+    logger.info("Starting bot")
+
+    config: Config = load_config()
+
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
